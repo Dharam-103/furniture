e334ab7645302da80601536ee6c8bb63ef3648cb
wave2211 <sagarikanikhade073@gmail.com>
2023-05-05 23:11:26 +0530

    singleProduct page

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import SingleProduct from "./Pages/Product/SingleProduct";
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <SingleProduct/>
     <Footer/>
     </div>
   );
