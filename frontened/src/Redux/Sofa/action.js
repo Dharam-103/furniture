d221b9e786fedf9b96250ca2117deae2fbe1ca5e
wave2211 <sagarikanikhade073@gmail.com>
2023-05-08 16:26:32 +0530

    done

---
+++ frontened/src/Redux/Sofa/action.js
@@ -0,0 +1,32 @@
+import { PRODUCT_FAILURE, PRODUCT_REQUEST, GET_PRODUCT_SUCCESS,} from "./actionType";
+import axios from "axios";
+
+
+export const getProducts = (paramobj) => (dispatch) =>{
+    dispatch({ type: PRODUCT_REQUEST })
+    axios
+        .get("https://odd-red-antelope-tux.cyclic.app/sofas", paramobj)
+        .then((res) => {
+            //  console.log(res.data)
+            dispatch({ type: GET_PRODUCT_SUCCESS , payload:res.data})
+        })
+        .catch(() => {
+            dispatch({ type: PRODUCT_FAILURE })
+        })
+}
+
+
+export const getSingleProduct = ( id) => (dispatch) =>{
+    dispatch({ type: PRODUCT_REQUEST })
+    return axios
+        .get(`https://odd-red-antelope-tux.cyclic.app/sofas/${id}`)
+        .then((res) => {
+             console.log(res.data)
+            dispatch({ type: GET_PRODUCT_SUCCESS})
+        })
+        .catch(() => {
+            dispatch({ type: PRODUCT_FAILURE })
+        })
+}
+
+
---
+++ frontened/src/Redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
+import {reducer as productReducer} from "./Sofa/reducer";
+
+const rootReducer = combineReducers({
+    productReducer
+})
+
+export const store = legacy_createStore(rootReducer , applyMiddleware(thunk))
