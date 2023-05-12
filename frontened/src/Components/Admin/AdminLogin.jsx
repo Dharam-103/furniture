19cc16d2b1d8ae64366682298b333c561498ada9
Jorden <dibyendunath001@gmail.com>
2023-05-13 01:24:51 +0530

    Admin Login Register Done

--- frontened/src/Components/Admin/AdminLogin.jsx
+++ frontened/src/Components/Admin/AdminLogin.jsx
@@ -1,136 +1,145 @@
-
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import {
-    Text,
-    Stack,
-    Flex,
-    Image,
-    Heading,
-    Checkbox,
-    Link,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputRightElement,
-    useToast
-  } from '@chakra-ui/react'
-import { useDisclosure } from '@chakra-ui/react-use-disclosure';
-import { VscAccount }  from 'react-icons/vsc';
-  import React, { useState }  from "react";
-import UserSignup from './UserSignup';
-
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function AdminLogin() {
-  const toast = useToast()
-    const [showPassword, setShowPassword] = useState(false);
-const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast();
+  const [showPassword, setShowPassword] = useState(false);
 
-const email = React.useRef(null)
-const password = React.useRef(null)
-const handleSubmit=(e)=>{
-  e.preventDefault();
-  let q1=email.current.value;
-  let q2=password.current.value
- 
-  const payload={email:q1,password:q2}
-  fetch("https://odd-red-antelope-tux.cyclic.app/admin/login",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(payload)
-        }).then((res)=>res.json())
-        .then((res)=>{
-          localStorage.setItem("token",res.token)
-          toast({
-            title: 'Login Successfull.',
-            description: "You are logged in.",
-            status: 'success',
-            duration: 9000,
-            isClosable: true,
-          })
-          console.log(res)        
-        })
-        .catch((err)=>console.log(err))
-        
-}
+  const email = React.useRef(null);
+  const password = React.useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let q1 = email.current.value;
+    let q2 = password.current.value;
 
-      
-        return (
-          <>
-          <VscAccount onClick={onOpen} fontSize={"25px"} />
-            <Modal
-              initialFocusRef={email}
-              finalFocusRef={password}
-              isOpen={isOpen}
-              onClose={onClose}
-              size={"5xl"}
-            >
-              <ModalOverlay />
-              <ModalContent >
-              <Stack minH={'80vh'} w={"auto"} direction={{ base: 'column', md: 'row' }}>
-              <Flex flex={1}>
-        <Image 
-          alt={'Login Image'}
-          objectFit={'cover'}
-          src={
-            'https://www.ulcdn.net/media/subscription_popup/SPop-Up-Soiree-in-the-Sun.jpg?1682879431'
-          }
-        />
-      </Flex>
-      <Flex p={8} flex={1} align={'center'} justify={'center'}>
-        <Stack spacing={4} w={'full'} maxW={'md'}>
-          <Heading fontSize={'2xl'}>Login to your account</Heading>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input ref={email} type="email" />
-          </FormControl>
-          
-          <FormControl id="password" isRequired>
+    const payload = { email: q1, password: q2 };
+    fetch("https://odd-red-antelope-tux.cyclic.app/admin/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("token", res.token);
+        toast({
+          title: "Login Successfull.",
+          description: "You are logged in.",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+        console.log(res);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  return (
+    <>
+      <Stack
+        maxH={"80vh"}
+        mt={"10"}
+        w={"auto"}
+        mb={"20"}
+        pb={"20"}
+        direction={{ base: "column", md: "row" }}
+      >
+        <Flex flex={1}>
+          <Image
+            alt={"Login Image"}
+            // flex={"3"}
+            h={"84vh"}
+            w={"65vw"}
+            mb={"6"}
+            objectFit={"fill"}
+            src={
+              "https://www.ulcdn.net/media/subscription_popup/SPop-Up-Soiree-in-the-Sun.jpg?1682879431"
+            }
+          />
+        </Flex>
+
+        <Flex
+          h={"75vh"}
+          w={"30vw"}
+          // flex={"2"}
+          pt={"8"}
+          align={"center"}
+          justify={"center"}
+        >
+          <Stack spacing={8} mt={"14"} mx={"auto"} maxW={"lg"} py={10} px={6}>
+            <Heading fontSize={"2xl"}>Login to Your Admin Account</Heading>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input ref={email} type="email" />
+            </FormControl>
+
+            <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input  ref={password} type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
+                <Input
+                  ref={password}
+                  type={showPassword ? "text" : "password"}
+                />
+                <InputRightElement h={"full"}>
                   <Button
-                    variant={'ghost'}
+                    variant={"ghost"}
                     onClick={() =>
                       setShowPassword((showPassword) => !showPassword)
-                    }>
+                    }
+                  >
                     {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
                 </InputRightElement>
               </InputGroup>
             </FormControl>
-          <Stack spacing={6}>
-            <Stack
-              direction={{ base: 'column', sm: 'row' }}
-              align={'start'}
-              justify={'space-between'}>
-              <Checkbox>Remember me</Checkbox>
-              <Link color={'orange.500'}>Forgot password?</Link>
-            </Stack>
-            <Button colorScheme={'orange'} variant={'solid'} onClick={handleSubmit}>
-              Login
-            </Button>
-            <Stack pt={6}>
-              <Text align={'center'} display={"flex"} m={"auto"}>
-              Not an Admin? <Link color={'orange.400'}><UserSignup/> </Link>
-              </Text>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link color={"orange.500"}>Forgot password?</Link>
+              </Stack>
+              <Button
+                colorScheme={"orange"}
+                variant={"solid"}
+                onClick={handleSubmit}
+              >
+                Login
+              </Button>
+              <Stack pt={6}>
+                <Text align={"center"} display={"flex"} m={"auto"}>
+                  Not an Admin ?
+                  <NavLink to="/adminregister" style={{ color: "orange" }}>
+                    {" "}
+                    <Text ml={"1"}>Register</Text>
+                  </NavLink>
+                </Text>
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </Flex>
-      
-    </Stack>
-              </ModalContent>
-            </Modal>
-          </>
-        )
-      
+        </Flex>
+      </Stack>
+    </>
+  );
 }
-export default AdminLogin
+export default AdminLogin;
--- frontened/src/Pages/UserLoginSignup/UserSignup.jsx
+++ frontened/src/Pages/UserLoginSignup/UserSignup.jsx
@@ -1,161 +1,187 @@
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    HStack,
-    InputRightElement,
-    Stack,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-    Link,
-    Image,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    useToast,
-  } from '@chakra-ui/react'
-import { useDisclosure } from '@chakra-ui/react-use-disclosure';
-import { Navigate } from "react-router-dom";
-  import React, { useState }  from "react";
-import axios from 'axios';
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Heading,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  Stack,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react-use-disclosure";
+import axios from "axios";
+import React, { useState } from "react";
 function UserSignup() {
-  const toast = useToast()
-    const [showPassword, setShowPassword] = useState(false);
-const { isOpen, onOpen, onClose } = useDisclosure()
-const [name,setName]=useState("")
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
-    const [phone,setPhone]=useState(0)
+  const toast = useToast();
+  const [showPassword, setShowPassword] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState(0);
 
-    const handleSubmit=()=>{
-        const payload={
-            name,email,password,phone
-        }
-        console.log(payload)
-        axios.post("https://odd-red-antelope-tux.cyclic.app/users/register",payload)
-        .then((res)=>{console.log(res)
-          toast({
-            title: 'Signup Successfull.',
-            description: "You are successfully signed in.",
-            status: 'success',
-            duration: 9000,
-            isClosable: true,
-          })
-        })
-        .catch((err)=>console.log(err))
-        
-    }
-          return (
-          <>
-          <Text onClick={onOpen} align={'center'}>
-                Sign up
-              </Text>
-            <Modal
-              initialFocusRef={name}
-              finalFocusRef={password}
-              isOpen={isOpen}
-              onClose={onClose}
-              size={"5xl"}
+  const handleSubmit = () => {
+    const payload = {
+      name,
+      email,
+      password,
+      phone,
+    };
+    console.log(payload);
+    axios
+      .post("https://odd-red-antelope-tux.cyclic.app/users/register", payload)
+      .then((res) => {
+        console.log(res);
+        toast({
+          title: "Signup Successfull.",
+          description: "You are successfully signed in.",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <>
+      <Text onClick={onOpen} align={"center"}>
+        Sign up
+      </Text>
+      <Modal
+        initialFocusRef={name}
+        finalFocusRef={password}
+        isOpen={isOpen}
+        onClose={onClose}
+        size={"5xl"}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <Stack
+            minH={"80vh"}
+            w={"auto"}
+            direction={{ base: "column", md: "row" }}
+          >
+            <Flex flex={1}>
+              <Image
+                alt={"Login Image"}
+                objectFit={"cover"}
+                src={
+                  "https://www.ulcdn.net/media/subscription_popup/SPop-Up-Soiree-in-the-Sun.jpg?1682879431"
+                }
+              />
+            </Flex>
+            <Flex
+              minH={"100vh"}
+              align={"center"}
+              justify={"center"}
+              bg={useColorModeValue("gray.50", "gray.800")}
             >
-              <ModalOverlay />
-              <ModalContent >
-              <Stack minH={'80vh'} w={"auto"} direction={{ base: 'column', md: 'row' }}>
-              <Flex flex={1}>
-        <Image
-          alt={'Login Image'}
-          objectFit={'cover'}
-          src={
-            'https://www.ulcdn.net/media/subscription_popup/SPop-Up-Soiree-in-the-Sun.jpg?1682879431'
-          }
-        />
-      </Flex>
-              <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'2xl'} textAlign={'center'}>
-          SIGN UP FOR SALE UPDATES
-          </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl id="firstName" isRequired>
-                  <FormLabel>First Name</FormLabel>
-                  <Input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName">
-                  <FormLabel>Contact</FormLabel>
-                  <Input type="number" value={phone} onChange={(e)=>setPhone(e.target.value)}/>
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button onClick={handleSubmit}
-                loadingText="Submitting"
-                size="lg"
-                bg={'orange.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user? <Link color={'orange.400'} >Login</Link>
-              </Text>
-            </Stack>
+              <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+                <Stack align={"center"}>
+                  <Heading fontSize={"2xl"} textAlign={"center"}>
+                    SIGN UP FOR SALE UPDATES
+                  </Heading>
+                  <Text fontSize={"lg"} color={"gray.600"}>
+                    to enjoy all of our cool features ✌️
+                  </Text>
+                </Stack>
+                <Box
+                  rounded={"lg"}
+                  bg={useColorModeValue("white", "gray.700")}
+                  boxShadow={"lg"}
+                  p={8}
+                >
+                  <Stack spacing={4}>
+                    <HStack>
+                      <Box>
+                        <FormControl id="firstName" isRequired>
+                          <FormLabel>First Name</FormLabel>
+                          <Input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                        </FormControl>
+                      </Box>
+                      <Box>
+                        <FormControl id="lastName">
+                          <FormLabel>Contact</FormLabel>
+                          <Input
+                            type="number"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                          />
+                        </FormControl>
+                      </Box>
+                    </HStack>
+                    <FormControl id="email" isRequired>
+                      <FormLabel>Email address</FormLabel>
+                      <Input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </FormControl>
+                    <FormControl id="password" isRequired>
+                      <FormLabel>Password</FormLabel>
+                      <InputGroup>
+                        <Input
+                          type={showPassword ? "text" : "password"}
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <InputRightElement h={"full"}>
+                          <Button
+                            variant={"ghost"}
+                            onClick={() =>
+                              setShowPassword((showPassword) => !showPassword)
+                            }
+                          >
+                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                    </FormControl>
+                    <Stack spacing={10} pt={2}>
+                      <Button
+                        onClick={handleSubmit}
+                        loadingText="Submitting"
+                        size="lg"
+                        bg={"orange.400"}
+                        color={"white"}
+                        _hover={{
+                          bg: "blue.500",
+                        }}
+                      >
+                        Sign up
+                      </Button>
+                    </Stack>
+                    <Stack pt={6}>
+                      <Text align={"center"}>
+                        Already a user? <Link color={"orange.400"}>Login</Link>
+                      </Text>
+                    </Stack>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Flex>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
-      
-    </Stack>
-              </ModalContent>
-            </Modal>
-          </>
-        )
-      
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
-export default UserSignup
+export default UserSignup;
